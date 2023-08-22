ddd94e310452520b7697da7558ee63cd27d19cec
Konrad Poweska <konradpoweska5@gmail.com>
2023-08-22 18:12:45 +0200

    feat: DB populating script

---
+++ src/scripts/initSampleData.ts
@@ -0,0 +1,31 @@
+import { clientPromise } from "$lib/server/db";
+import { homes } from "$lib/server/home/model";
+import { towels } from "$lib/server/towels/model";
+import { users } from "$lib/server/users/model";
+
+const client = await clientPromise;
+
+// drop collections ignoring errors
+await Promise.allSettled([homes, towels].map((coll) => coll.drop()));
+
+let user = await users.findOne();
+if (user === null) throw new Error("No user in database");
+let { _id: userId } = user;
+
+let home = await homes.insertOne({
+  name: "Home 1",
+  members: [userId],
+  towels: ["Beżowy"],
+});
+
+let yesterday = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+
+let towel = await towels.insertOne({
+  usedBy: userId,
+  name: "Beżowy",
+  usedSince: yesterday,
+  home: home.insertedId,
+});
+
+await client.close();
