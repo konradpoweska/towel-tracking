9d2e51d768b3d17293f0d940d2ffa45b6857d5b9
Konrad Poweska <konradpoweska5@gmail.com>
2024-11-11 19:23:02 +0100

    fix(db): type checks in CI

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,9 +2,13 @@ import { building } from "$app/environment";
 import { env } from "$env/dynamic/private";
 import { MongoClient, ObjectId } from "mongodb";
 
-const client = new MongoClient(
-  building ? "mongodb://dummy-host/" : env.MONGO_URL
-);
+const getMongoUrl = (): string => {
+  if (building) return "mongodb://dummy-host/";
+  if (!env.MONGO_URL) throw new Error("MONGO_URL not defined");
+  return env.MONGO_URL;
+};
+
+const client = new MongoClient(getMongoUrl());
 export const clientPromise = client.connect();
 
 export const database = client.db(env.MONGO_DB);
